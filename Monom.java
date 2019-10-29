6cb9e53a793a744ca87e2eb1945c76cbef971ef3
UrielAbergel <54840897+UrielAbergel@users.noreply.github.com>
2019-10-29 21:16:47 +0200

    first commit

---
+++ Monom.java
@@ -0,0 +1,106 @@
+import java.util.Comparator;
+
+/**
+ * This class represents a simple "Monom" of shape a*x^b, where a is a real number and a is an integer (summed a none negative), 
+ * see: https://en.wikipedia.org/wiki/Monomial 
+ * The class implements function and support simple operations as: construction, value at x, derivative, add and multiply. 
+ * @author Boaz
+ *
+ */
+public class Monom implements function{
+	public static final Monom ZERO = new Monom(0,0);
+	public static final Monom MINUS1 = new Monom(-1,0);
+	public static final double EPSILON = 0.0000001;
+	public static final Comparator<Monom> _Comp = new Monom_Comperator();
+	public static Comparator<Monom> getComp() {return _Comp;}
+	public Monom(double a, int b){
+		this.set_coefficient(a);
+		this.set_power(b);
+	}
+	public Monom(Monom ot) {
+		this(ot.get_coefficient(), ot.get_power());
+	}
+
+	public double get_coefficient() {
+		return this._coefficient;
+	}
+	public int get_power() {
+		return this._power;
+	}
+	/**
+	 * this method returns the derivative monom of this.
+	 * @return
+	 */
+	public Monom derivative() {
+		if(this.get_power()==0) {return getNewZeroMonom();}
+		return new Monom(this.get_coefficient()*this.get_power(), this.get_power()-1);
+	}
+	public double f(double x) {
+		double ans=0;
+		double p = this.get_power();
+		ans = this.get_coefficient()*Math.pow(x, p);
+		return ans;
+	}
+	public boolean isZero() {return this.get_coefficient() == 0;}
+	// ***************** add your code below **********************
+	public Monom(String s) {
+		String num = "";
+		String pow = "";
+		int x = 0;
+		while(x<=s.length()) {
+			if (s.charAt(x) == 'x' || s.charAt(x) == 'X') break;
+			num = num + s.charAt(x++);
+		}
+		double mekadem = 0;
+		if(s.charAt(0) == 'x' || s.charAt(0) == 'X') mekadem = 1;
+		else if((s.charAt(0) == '+' && s.charAt(0) == '-') && (s.charAt(1) == 'x' || s.charAt(1) == 'X')) mekadem = -1;
+		else mekadem = Double.parseDouble(num);
+
+		x=x+2;
+		while(x < s.length()){
+			pow = pow + s.charAt(x);
+			x++;
+		}
+		if(pow.isEmpty()) {pow = "1";}
+		int power = Integer.parseInt(pow);
+
+		this._power = power ;
+		this._coefficient = mekadem;
+	}
+
+	public void add(Monom m) {
+		this._coefficient = this._coefficient + m._coefficient;
+	}
+
+	public void multipy(Monom d) {
+		this._coefficient = this._coefficient*d._coefficient;
+		this._power = this._power + d._power;
+	}
+
+	public String toString() {
+		return this._coefficient + "x^" + this._power;
+	}
+	// you may (always) add other methods.
+
+	//****************** Private Methods and Data *****************
+
+
+	private void set_coefficient(double a){
+		this._coefficient = a;
+	}
+	private void set_power(int p) {
+		if(p<0) {throw new RuntimeException("ERR the power of Monom should not be negative, got: "+p);}
+		this._power = p;
+	}
+	private static Monom getNewZeroMonom() {return new Monom(ZERO);}
+	private double _coefficient;
+	private int _power;
+
+	public static void main(String[] args) {
+		String s = "+12.59x^4";
+		Monom a = new Monom(s);
+		System.out.println(a._coefficient);
+		System.out.println(a._power);
+	}
+
+}
---
+++ Polynom.java
@@ -0,0 +1,130 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+
+/**
+ * This class represents a Polynom with add, multiply functionality, it also should support the following:
+ * 1. Riemann's Integral: https://en.wikipedia.org/wiki/Riemann_integral
+ * 2. Finding a numerical value between two values (currently support root only f(x)=0).
+ * 3. Derivative
+ * 
+ * @author Boaz
+ *
+ */
+public class Polynom implements Polynom_able{
+
+	/**
+	 * Zero (empty polynom)
+	 */
+	public Polynom() {
+
+	}
+	/**
+	 * init a Polynom from a String such as:
+	 *  {"x", "3+1.4X^3-34x", "(2x^2-4)*(-1.2x-7.1)", "(3-3.4x+1)*((3.1x-1.2)-(3X^2-3.1))"};
+	 * @param s: is a string represents a Polynom
+	 */
+
+	public Polynom(String s) {
+		String t = "";
+		Monom[] arr = new Monom[s.length()];
+		int Startindex  = 0 ;
+		int EndIndex = 0 ;
+		int arrIndex = 0;
+		int JustCheck = 0  ;
+		for (int i = 0 ; i < s.length() ; i++) {
+			if (s.charAt(i) == '(' || s.charAt(i) == ')') {
+			EndIndex = s.length()+1	;
+			}
+		}
+			while (EndIndex < s.length()) {
+				if (EndIndex != 0) {
+					if (EndIndex == '+' || EndIndex == '-') {
+						arr[arrIndex] = new Monom(s.substring(Startindex, EndIndex));
+						arrIndex++;
+						Startindex = EndIndex;
+					}
+				}
+				EndIndex++;
+			}
+
+	}
+	@Override
+	public double f(double x) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void add(Polynom_able p1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Monom m1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void substract(Polynom_able p1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void multiply(Polynom_able p1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean equals(Polynom_able p1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isZero() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double root(double x0, double x1, double eps) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Polynom_able copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Polynom_able derivative() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double area(double x0, double x1, double eps) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<Monom> iteretor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void multiply(Monom m1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
