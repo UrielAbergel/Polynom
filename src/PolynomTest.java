d14ef169802802f39396451e8dc8dfb64fca1928
UrielAbergel <54840897+UrielAbergel@users.noreply.github.com>
2019-11-25 22:31:45 +0200

    add junit test to the file

---
+++ src/PolynomTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolynomTest {
+
+    @Test
+    void f() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void substract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void root() {
+    }
+
+    @Test
+    void copy() {
+    }
+
+    @Test
+    void derivative() {
+    }
+
+    @Test
+    void area() {
+    }
+}
