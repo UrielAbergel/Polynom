8d94bd7d6ed38a186166dfa4949c8efb0c306bb8
UrielAbergel <54840897+UrielAbergel@users.noreply.github.com>
2019-11-27 11:28:10 +0200

    add the new porject files and change the interface

--- src/MonomTest.java
+++ src/MonomTest.java
@@ -6,6 +6,14 @@ class MonomTest {
 
     @Test
     void derivative() {
+        double ans ;
+        Monom test1 = new Monom("5x^3");
+        Monom test2 = new Monom("x");
+        Monom test3 = new Monom("x^2");
+        test1.derivative();
+        test2.derivative();
+        test3.derivative();
+
     }
 
     @Test
--- src/cont_function.java
+++ src/cont_function.java
@@ -1,18 +1,19 @@
-/**
- * The interface represents a continuance function 
+
+ /**
+ * The interface represents a continuance function
  * @author Boaz
  *
- */ 
+ */
 public interface cont_function extends function{
 	/**
-	 * Compute a Riman's integral from x0 to x1 in eps steps. 
-	 * @param x0 starting pooint
+	 * Compute a Riman's integral from x0 to x1 in eps steps.
+	 * @param x0 starting point
 	 * @param x1 end point
 	 * @param eps positive step value
 	 * @return the approximated area above X-axis below this function bounded in the range of [x0,x1]
 	 */
 	public double area(double x0, double x1, double eps);
-	
+
 	/**
 	 * Compute a value x' (x0<=x'<=x1) for with |f(x')| < eps
 	 * assuming (f(x0)*f(x1)<=0, else should throws runtimeException 
