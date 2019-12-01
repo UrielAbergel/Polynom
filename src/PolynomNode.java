19b27ab70f6f940132f269315a4391a393c6b071
YairSemama <55833857+YairSemama@users.noreply.github.com>
2019-12-01 22:58:50 +0200

    start the Binary tree

--- src/PolynomNode.java
+++ src/PolynomNode.java
@@ -1,13 +1,19 @@
 public class PolynomNode {
-
+    Monom m;
     Polynom p;
-    PolynomNode left;
-    PolynomNode right;
+    PolynomNode left, right, father;
+    Operation OP;
+    function f;
 
+    PolynomNode(Object p) {
+        if(p instanceof Polynom){
+            Polynom p1 = (Polynom)(p);
+            new PolynomNode(p1);
+        }
+    }
     PolynomNode(Polynom p){
         this.p.PolynomList = p.PolynomList;
         left = null;
         right = null;
     }
-
 }
