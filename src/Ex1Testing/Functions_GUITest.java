01b38516b8c2d5aa2c62a2398dc803f6a16dbd54
YairSemama <55833857+YairSemama@users.noreply.github.com>
2019-12-10 21:31:25 +0200

    night push

--- src/Ex1Testing/Functions_GUITest.java
+++ src/Ex1Testing/Functions_GUITest.java
@@ -1,20 +1,15 @@
 package Ex1Testing;
 
-import java.io.IOException;
-import java.util.Iterator;
-
+import Ex1.*;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import Ex1.Functions_GUI;
-import Ex1.ComplexFunction;
-import Ex1.Functions_GUI;
-import Ex1.Monom;
-import Ex1.Operation;
-import Ex1.Polynom;
-import Ex1.Range;
-import Ex1.function;
-import Ex1.functions;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+
+//import org.junit.jupiter.api.Test;
 /**
  * Note: minor changes (thanks to Amichai!!)
  * The use of "get" was replaced by iterator!
@@ -32,116 +27,117 @@ import Ex1.functions;
  *
  */
 class Functions_GUITest {
-	public static void main(String[] a) {
-		functions data = FunctionsFactory();
-		int w=1000, h=600, res=200;
-		Range rx = new Range(-10,10);
-		Range ry = new Range(-5,15);
-		data.drawFunctions(w,h,rx,ry,res);
-		String file = "function_file.txt";
-		String file2 = "function_file2.txt";
-		try {
-			data.saveToFile(file);
-			Functions_GUI data2 = new Functions_GUI();
-			data2.initFromFile(file);
-			data.saveToFile(file2);
-		}
-		catch(Exception e) {e.printStackTrace();}
-		String JSON_param_file = "GUI_params.txt";
-		data.drawFunctions(JSON_param_file);
-	}
-	private functions _data=null;
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		int width = 900;
-		int height = 900;
-		int res = 200;
-		Range rx = new Range(-15,15);
-		Range ry = new Range(-15,15);
-	}
+    public static void main(String[] a) {
+        functions data = FunctionsFactory();
+        int w=1000, h=600, res=200;
+        Range rx = new Range(-10,10);
+        Range ry = new Range(-5,15);
+        data.drawFunctions(w,h,rx,ry,res);
+        String file = "function_file.txt";
+        String file2 = "function_file2.txt";
+        try {
+            data.saveToFile(file);
+            Functions_GUI data2 = new Functions_GUI();
+            data2.initFromFile(file);
+            data.saveToFile(file2);
+        }
+        catch(Exception e) {e.printStackTrace();}
+        String JSON_param_file = "GUI_params.txt";
+        data.drawFunctions(JSON_param_file);
+    }
+    private functions _data=null;
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        int width = 900;
+        int height = 900;
+        int res = 200;
+        Range rx = new Range(-15,15);
+        Range ry = new Range(-15,15);
+    }
 
-	@BeforeEach
-	void setUp() throws Exception {
-		_data = FunctionsFactory();
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+        _data = FunctionsFactory();
+    }
 
-	@Test
-	void testFunctions_GUI() {
+    @Test
+    void testFunctions_GUI() {
 
-		//fail("Not yet implemented");
-	}
+        //fail("Not yet implemented");
+    }
 
-	@Test
-	void testInitFromFile() {
-		String path = "function_file.txt";
-		Functions_GUI f_GUI = new Functions_GUI();
-		try {
-			f_GUI.initFromFile(path);
-		} catch (Exception e) {
-			System.out.println("Your path is leading to nowhere");
-		}
-		ComplexFunction cf = new ComplexFunction();
-		function f1 = cf.initFromString("");
-		function f2 = cf.initFromString("");
-		function f3 = cf.initFromString("");
-		function f4 = cf.initFromString("");
-		function f5 = cf.initFromString("");
-		function f6 = cf.initFromString("");
-		function f7 = cf.initFromString("");
-	}
+    @Test
+    void testInitFromFile() {
+        String path = "function_file.txt";
+        Functions_GUI f_GUI = new Functions_GUI();
+        try {
+            f_GUI.initFromFile(path);
+        } catch (Exception e) {
+            System.out.println("Your path is leading to nowhere");
+        }
+        ComplexFunction cf = new ComplexFunction();
+        function f1 = cf.initFromString("Plus(-1.0x^4+3.1x^0,0.1x^5-1.2999999999999998x^1+5.0x^0)");
+        function f2 = cf.initFromString("Plus(Divid(1.0x^1+1.0x^0,Times(Times(1.0x^1-2.0x^0,0.0x^0),1.0x^1-4.0x^0)),2.0x^0)");
+        function f3 = cf.initFromString("Divid(Plus(-1.0x^4+3.1x^0,0.1x^5-1.2999999999999998x^1+5.0x^0),-1.0x^4+3.1x^0)");
+        function f4 = cf.initFromString("-1.0x^4+2.4x^2+3.1x^0");
+        function f5 = cf.initFromString("0.1x^5-1.2999999999999998x^1+5.0x^0");
+        function f6 = cf.initFromString("Max(Max(Max(Max(Plus(-1.0x^4+3.1x^0,0.1x^5-1.2999999999999998x^1+5.0x^0),Plus(Divid(1.0x^1+1.0x^0,Times(Times(1.0x^1-2.0x^0,0.0x^0),1.0x^1-4.0x^0)),2.0x^0)),Divid(Plus(-1.0x^4+3.1x^0,0.1x^5-1.2999999999999998x^1+5.0x^0),-1.0x^4+3.1x^0)),-1.0x^4+2.4x^2+3.1x^0),0.1x^5-1.2999999999999998x^1+5.0x^0)");
+        function f7 = cf.initFromString("Min(Min(Min(Min(Plus(-1.0x^4+3.1x^0,0.1x^5-1.2999999999999998x^1+5.0x^0),Plus(Divid(1.0x^1+1.0x^0,Times(Times(1.0x^1-2.0x^0,0.0x^0),1.0x^1-4.0x^0)),2.0x^0)),Divid(Plus(-1.0x^4+3.1x^0,0.1x^5-1.2999999999999998x^1+5.0x^0),-1.0x^4+3.1x^0)),-1.0x^4+2.4x^2+3.1x^0),0.1x^5-1.2999999999999998x^1+5.0x^0)");
+        assertEquals(f1.toString(),f_GUI.get(0).toString());
+    }
 
-	@Test
-	void testSaveToFile() {
+    @Test
+    void testSaveToFile() {
 
-	}
+    }
 
-	@Test
-	void testDrawFunctions() {
+    @Test
+    void testDrawFunctions() {
 //		_data.drawFunctions();
 //		fail("Not yet implemented");
-	}
+    }
 
-	@Test
-	void testDrawFunctionsIntIntRangeRangeInt() {
-		_data.drawFunctions("GUI_params.txt");
-		//fail("Not yet implemented");
-	}
-	public static functions FunctionsFactory() {
-		functions ans = new Functions_GUI();
-		String s1 = "3.1 +2.4x^2 -x^4";
-		String s2 = "5 +2x -3.3x +0.1x^5";
-		String[] s3 = {"x +3","x -2", "x -4"};
-		Polynom p1 = new Polynom(s1);
-		Polynom p2 = new Polynom(s2);
-		Polynom p3 = new Polynom(s3[0]);
-		ComplexFunction cf3 = new ComplexFunction(p3);
-		for(int i=1;i<s3.length;i++) {
-			cf3.mul(new Polynom(s3[i]));
-		}
+    @Test
+    void testDrawFunctionsIntIntRangeRangeInt() {
+        _data.drawFunctions("GUI_params.txt");
+        //fail("Not yet implemented");
+    }
+    public static functions FunctionsFactory() {
+        functions ans = new Functions_GUI();
+        String s1 = "3.1 +2.4x^2 -x^4";
+        String s2 = "5 +2x -3.3x +0.1x^5";
+        String[] s3 = {"x +3","x -2", "x -4"};
+        Polynom p1 = new Polynom(s1);
+        Polynom p2 = new Polynom(s2);
+        Polynom p3 = new Polynom(s3[0]);
+        ComplexFunction cf3 = new ComplexFunction(p3);
+        for(int i=1;i<s3.length;i++) {
+            cf3.mul(new Polynom(s3[i]));
+        }
 
-		ComplexFunction cf = new ComplexFunction(Operation.Plus, p1,p2);
-		ComplexFunction cf4 = new ComplexFunction("div", new Polynom("x +1"),cf3);
-		cf4.plus(new Monom("2"));
-		ans.add(cf.copy());
-		ans.add(cf4.copy());
-		cf.div(p1);
-		ans.add(cf.copy());
-		String s = cf.toString();
-		function cf5 = cf4.initFromString(s1);
-		function cf6 = cf4.initFromString(s2);
-		ans.add(cf5.copy());
-		ans.add(cf6.copy());
-		Iterator<function> iter = ans.iterator();
-		function f = iter.next();
-		ComplexFunction max = new ComplexFunction(f);
-		ComplexFunction min = new ComplexFunction(f);
-		while(iter.hasNext()) {
-			f = iter.next();
-			max.max(f);
-			min.min(f);
-		}
-		ans.add(max);
-		ans.add(min);
-		return ans;
-	}
+        ComplexFunction cf = new ComplexFunction(Operation.Plus, p1,p2);
+        ComplexFunction cf4 = new ComplexFunction("div", new Polynom("x +1"),cf3);
+        cf4.plus(new Monom("2"));
+        ans.add(cf.copy());
+        ans.add(cf4.copy());
+        cf.div(p1);
+        ans.add(cf.copy());
+        String s = cf.toString();
+        function cf5 = cf4.initFromString(s1);
+        function cf6 = cf4.initFromString(s2);
+        ans.add(cf5.copy());
+        ans.add(cf6.copy());
+        Iterator<function> iter = ans.iterator();
+        function f = iter.next();
+        ComplexFunction max = new ComplexFunction(f);
+        ComplexFunction min = new ComplexFunction(f);
+        while(iter.hasNext()) {
+            f = iter.next();
+            max.max(f);
+            min.min(f);
+        }
+        ans.add(max);
+        ans.add(min);
+        return ans;
+    }
 }
